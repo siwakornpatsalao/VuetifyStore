afbce5fa1faeded53a6cb20910a6923955c1357c
siwakornpatsalao <siwakorn.pat@ku.th>
2023-05-19 17:27:44 +0700

    add dist

---
+++ dist/_nuxt/1b9f947.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{566:function(n,e,d){"use strict";d.r(e);var l=d(90),component=Object(l.a)({},undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
